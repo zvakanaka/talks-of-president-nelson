2b64e5237f9255005965867adbe510f69bee608c
zvakanaka <quintonish@gmail.com>
2020-03-26 21:28:08 -0600

    Updates

---
+++ precache-manifest.1fc7a2e41f545ec24c092467f1796faf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34977f17ebcac36a23f1e281c106842",
+    "url": "/talks-of-president-nelson/index.html"
+  },
+  {
+    "revision": "1edf818309a42c92adda",
+    "url": "/talks-of-president-nelson/static/js/2.29cae8e5.chunk.js"
+  },
+  {
+    "revision": "3032fca56698782080dd",
+    "url": "/talks-of-president-nelson/static/js/main.747ef475.chunk.js"
+  },
+  {
+    "revision": "2c20ed6486f3e8442cf1",
+    "url": "/talks-of-president-nelson/static/js/runtime~main.96190075.js"
+  }
+]);
