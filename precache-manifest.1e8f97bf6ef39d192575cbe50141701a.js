9d7760ab75f016431f7e865cd2e28b619044bb32
Adam Quinton <quintonish@gmail.com>
2021-08-09 23:27:56 -0600

    Updates

--- precache-manifest.1e8f97bf6ef39d192575cbe50141701a.js
+++ precache-manifest.1e8f97bf6ef39d192575cbe50141701a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4f5bb3c9ad7bed109b208cdbc1f6d62",
+    "revision": "3101b1869f9dd40495f2557af813d84b",
     "url": "/talks-of-president-nelson/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/talks-of-president-nelson/static/js/2.29cae8e5.chunk.js"
   },
   {
-    "revision": "5bba3dca40c0bd789828",
-    "url": "/talks-of-president-nelson/static/js/main.3efae61e.chunk.js"
+    "revision": "5ed9d5c7021e899c3cf1",
+    "url": "/talks-of-president-nelson/static/js/main.409d06d5.chunk.js"
   },
   {
     "revision": "2c20ed6486f3e8442cf1",
