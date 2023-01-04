077f229be2f3eb871f4421755bcb40935d835daa
Adam <8365885+zvakanaka@users.noreply.github.com>
2023-01-03 20:36:20 -0700

    Updates

---
+++ precache-manifest.fb8e9c605943badb8fe5de57edf9759c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e663a216febd87f7389190f2e5afbe2",
+    "url": "/talks-of-president-nelson/index.html"
+  },
+  {
+    "revision": "ed3399db328005c95e14",
+    "url": "/talks-of-president-nelson/static/js/2.f71181f4.chunk.js"
+  },
+  {
+    "revision": "1a6e923eec84f4ae841e",
+    "url": "/talks-of-president-nelson/static/js/main.7cbeab2f.chunk.js"
+  },
+  {
+    "revision": "2c20ed6486f3e8442cf1",
+    "url": "/talks-of-president-nelson/static/js/runtime~main.96190075.js"
+  }
+]);
